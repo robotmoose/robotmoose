4b8f8b9a83ab1ac6a70b9b8eb4f8b254af054f14
Dr. Orion Lawlor <lawlor@alaska.edu>
2014-09-30 16:28:45 -0800

    Benchmark script now runs for a fixed 10 seconds, and then does analysis.

--- superstar/mongoose/bench_all.sh
+++ superstar/mongoose/bench_all.sh
@@ -5,11 +5,17 @@ make clean all || exit 1
 echo "Starting server"
 ./demo > log_demo &
 
-echo "Starting clients.  Press ctrl-C to exit test."
-sleep 1
+echo "Starting clients.  We'll run for 10 seconds."
 cd latency_analysis/benchmark
 	rm copy_*
-export TIMEFORMAT="%4R"
+export TIMEFORMAT="%5R"
       ./bench.sh 2> copy_1 &
       ./bench.sh 2> copy_2 &
-      ./bench.sh 2> copy_3
+      ./bench.sh 2> copy_3 &
+cd ../..
+
+sleep 10
+killall demo
+killall bench.sh
+./bench_dump.sh
+
--- superstar/mongoose/bench_dump.sh
+++ superstar/mongoose/bench_dump.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-killall bench.sh wget
-
 gr="grep -hv URL latency_analysis/benchmark/copy*"
 
 echo "Statistics:"
@@ -16,8 +14,9 @@ $gr | perl -e 'use List::Util qw(max min sum);
 	$std=sqrt($sqsum/$n-($s/$n)*($s/$n));
 	$mid=int @a/2;
 	@srtd=sort @a;
-	if(@a%2){$med=$srtd[$mid];}else{$med=($srtd[$mid-1]+$srtd[$mid])/2;};
+	if(@a%2){$med=($srtd[$mid])*1;}else{$med=($srtd[$mid-1]+$srtd[$mid])/2;};
 	print "records:$n\nsum:$s\navg:$a\nstd:$std\nmed:$med\nmax:$m\min:$mm";'
 
+echo
 echo "Worst ten timings:"
 $gr | sort | tail -10
