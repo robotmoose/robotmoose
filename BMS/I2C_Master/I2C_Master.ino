37c3bc762ab742c8a155e493ab755d4165bb6dc5
Clayton Auld <clayauld@gmail.com>
2014-12-07 21:36:11 -0900

    Added I2C functionality to BMS. However, it is still not working properly.

--- BMS/I2C_Master/I2C_Master.ino
+++ BMS/I2C_Master/I2C_Master.ino
@@ -12,13 +12,36 @@
 
 #include <Wire.h>
 
+byte x=0;
+
 void setup()
 {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
 }
 
+
 void loop()
+{
+  Serial.println(x);
+  Wire.beginTransmission(2);
+  Wire.write(x);
+  Wire.endTransmission();
+  Wire.requestFrom(2, 6);    // request 6 bytes from slave device #2
+
+  while(Wire.available())    // slave may send less than requested
+  { 
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+  Serial.println("");
+  x++;
+  if (x>2) x=0;
+  delay(2000);
+}
+
+
+/*void loop()
 {
   Wire.requestFrom(2, 6);    // request 6 bytes from slave device #2
 
@@ -27,6 +50,7 @@ void loop()
     char c = Wire.read(); // receive a byte as character
     Serial.print(c);         // print the character
   }
+  Serial.println("");
 
-  delay(500);
-}
+  delay(1200);
+}*/
