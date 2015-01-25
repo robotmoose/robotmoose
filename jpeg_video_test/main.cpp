22453eb97e7cff6e9dc28f8ed3e142a1f8fc88de
Your Name <you@example.com>
2015-01-24 16:35:08 -0900

    Got video over http with uri parser for quality and not *horrible* framerate...

--- jpeg_video_test/main.cpp
+++ jpeg_video_test/main.cpp
@@ -3,8 +3,44 @@
 #include <msl/webserver.hpp>
 #include "webcam.hpp"
 #include <fstream>
+#include <uriparser/Uri.h>
+#include <stdexcept>
 
+void send_jpg(const mg_connection& connection,const std::string& jpg);
+
+bool client_func(const mg_connection& connection,enum mg_event event);
+
+msl::webserver_t server(client_func,"0.0.0.0:80","web");
 webcam_t cam;
+
+int main()
+{
+	server.open();
+
+	if(server.good())
+		std::cout<<":)"<<std::endl;
+	else
+		std::cout<<":("<<std::endl;
+
+	while(server.good())
+	{
+		size_t num=0;
+		cam.open(num);
+
+		if(cam.good())
+			std::cout<<"Connected with camera "<<num<<"."<<std::endl;
+
+		while(cam.good())
+			msl::delay_ms(1);
+
+		std::cout<<"Lost connection with camera "<<num<<"."<<std::endl;
+		msl::delay_ms(1);
+	}
+
+	server.close();
+
+	return 0;
+}
 
 void send_jpg(const mg_connection& connection,const std::string& jpg)
 {
@@ -20,44 +56,58 @@ bool client_func(const mg_connection& connection,enum mg_event event)
 	if(event!=MG_REQUEST)
 		return false;
 
-	std::string request=connection.uri;
 
-	if(request=="/cam.jpg")
+	if(std::string(connection.uri)=="/cam.jpg")
 	{
-		send_jpg(connection,cam.jpg(30));
+		size_t quality=30;
+
+		if(connection.query_string!=nullptr)
+		{
+			std::string request="?"+std::string(connection.query_string);
+
+			UriParserStateA state;
+			UriUriA uri;
+			state.uri=&uri;
+
+			if(uriParseUriA(&state,request.c_str())!=0)
+			{
+				std::cout<<"failed to parse \""<<request<<"\"!"<<std::endl;
+				uriFreeUriMembersA(&uri);
+				return false;
+			}
+
+			UriQueryListA* queries;
+
+			if(uriDissectQueryMallocA(&queries,nullptr,uri.query.first,uri.query.afterLast)!=0)
+			{
+				std::cout<<"failed to decode \""<<request<<"\"!"<<std::endl;
+				uriFreeUriMembersA(&uri);
+				return false;
+			}
+
+			auto head=queries;
+
+			while(head!=nullptr&&head->key!=nullptr&&head->value!=nullptr)
+			{
+				std::string key=head->key;
+				std::string value=head->value;
+
+				if(key=="quality")
+				{
+					try{quality=std::stoi(value);}
+					catch(...){}
+				}
+
+				head=head->next;
+			}
+
+			uriFreeQueryListA(queries);
+			uriFreeUriMembersA(&uri);
+		}
+
+		send_jpg(connection,cam.jpg(quality));
 		return true;
 	}
 
-	std::cout<<request<<std::endl;
 	return false;
-}
-
-int main()
-{
-	msl::webserver_t test(client_func,"0.0.0.0:8080","web");
-	test.open();
-
-	if(test.good())
-		std::cout<<":)"<<std::endl;
-	else
-		std::cout<<":("<<std::endl;
-
-	while(test.good())
-	{
-		size_t num=0;
-		cam.open(num);
-
-		if(cam.good())
-			std::cout<<"Connected with camera "<<num<<"."<<std::endl;
-
-		while(cam.good())
-			msl::delay_ms(1);
-
-		std::cout<<"Lost connection with camera "<<num<<"."<<std::endl;
-		msl::delay_ms(1);
-	}
-
-	test.close();
-
-	return 0;
 }
