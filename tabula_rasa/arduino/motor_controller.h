1458cf865ed12b6402ad4b7605a979a8a8dbc229
Arsh Chauhan <achauhan@alaska.edu>
2015-03-17 16:02:25 -0800

    Added motor controller prototypes

--- tabula_rasa/arduino/motor_controller.h
+++ tabula_rasa/arduino/motor_controller.h
@@ -27,4 +27,34 @@ class bts_controller_t:public motor_controller_t
 		uint16_t right_pwm_pin_m;
 };
 
+class sabertooth_controller_t:public motor_controller_t
+{
+	public:
+		sabertooth_controller_t(const int16_t serial_pin_RX,const int16_t serial_pin_TX, const int16_t baud);
+		void setup();
+		void drive(const int16_t left, const int16_t right);
+		
+	private:
+		uint16_t serial_pin_RX;
+		uint16_t serial_pin_TX;
+		uint16_t baud;
+};
+
+class create2_controller_t:public motor_controller_t
+{
+	public:
+		create2_controller_t(const int16_t serial_pin_RX,const int16_t serial_pin_TX, const int16_t baud);
+		void setup();
+		void drive(const int16_t left,const int16_t right);
+	
+	private:
+		uint16_t serial_pin_RX;
+		uint16_t serial_pin_TX;
+		uint16_t baud;
+};
+
+
+	
+		
+
 #endif
