40f1b00477599467693f95657fd0ab5848fdfaa1
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-03-17 18:16:37 -0800

    Removed battery copy constructors, which didn't work, and weren't needed.

--- tabula_rasa/arduino/robot.cpp
+++ tabula_rasa/arduino/robot.cpp
@@ -7,30 +7,7 @@ battery_t::battery_t(const uint16_t cell_count):cell_count(cell_count),
 		cells[ii]=0.0;
 }
 
-battery_t::battery_t(const battery_t& copy)
-{
-	free(cells);
-	cell_count=copy.cell_count;
-
-	for(uint16_t ii=0;ii<cell_count;++ii)
-		cells[ii]=copy.cells[ii];
-}
-
 battery_t::~battery_t()
 {
 	free(cells);
-}
-
-battery_t& battery_t::operator=(const battery_t& copy)
-{
-	if(this!=&copy)
-	{
-		free(cells);
-		cell_count=copy.cell_count;
-
-		for(uint16_t ii=0;ii<cell_count;++ii)
-			cells[ii]=copy.cells[ii];
-	}
-
-	return *this;
 }
