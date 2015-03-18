1753a4908a9f01a931fc2e1129161b5f0f3ef64a
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-03-17 17:29:06 -0800

    Use new "action" madness. Demo only, real version should merge action and motor_controller.

--- tabula_rasa/arduino/arduino.ino
+++ tabula_rasa/arduino/arduino.ino
@@ -1,13 +1,26 @@
+#include "action.h"
 #include "motor_controller.h"
 
 uint16_t right_pins[2]={3,5};
 uint16_t left_pins[2]={9,6};
 
-bts_controller_t test(left_pins,right_pins);
+class motor_action : public action {
+{
+public:
+	bts_controller_t ctl;
+	motor_action() :ctl(left_pins,right_pins) 
+	{
+		ctl.setup();
+	}
+	void loop() {
+		test.drive(0,0);
+	}
+};	
 
 void setup()
 {
-  test.setup();
+  action_setup();
+  action_10ms.add(new motor_action());
 
   for(int ii=16;ii<=19;++ii)
   {
@@ -18,7 +31,7 @@ void setup()
 
 void loop()
 {
-  test.drive(0,0);
+  action_loop();
 }
 
 
