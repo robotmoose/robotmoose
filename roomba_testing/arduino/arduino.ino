fbb438d8d4dd3ea245e410b39c372cc1d8f0b552
Your Name <you@example.com>
2015-03-19 15:57:48 -0800

    Fixed roomba stuff...what...who knows...

--- roomba_testing/arduino/arduino.ino
+++ roomba_testing/arduino/arduino.ino
@@ -1,5 +1,16 @@
+#include "roomba.h"
+
+roomba_serial_t roomba_serial(Serial3);
+roomba_t roomba(roomba_serial);
+
 void setup()
 {
+  Serial.begin(57600);
+  Serial3.begin(115200);
+  roomba.reset();
+  roomba.start();
+  roomba.set_mode(roomba_t::FULL);
+  roomba.drive(100,0);
 }
 
 void loop()
