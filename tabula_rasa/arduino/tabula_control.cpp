71e89a989c24154e1ee75c306d1f603dfabafe35
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-03-22 00:13:42 -0800

    Removed leftover comment and header at bottom of tabula_control.

--- tabula_rasa/arduino/tabula_control.cpp
+++ tabula_rasa/arduino/tabula_control.cpp
@@ -95,19 +95,3 @@ REGISTER_TABULA_DEVICE(serial_controller,
 	actions_10ms.add(new serial_controller(Serial));
 )
 
-
-/**
- Scalable command and sense system for robot control:
-
-command: PC sending control commands for motor controllers, 
-  enable bits, etc.
-
-sensor: Arduino sending temperature, ultrasonic, bumpers, etc.
-
- Dr. Orion Lawlor, lawlor@alaska.edu, 2015-03-18 (Public Domain)
-*/
-#include "tabula_control.h"
-#include "tabula_config.h"
-
-
-// Example devices moved to devices.cpp
