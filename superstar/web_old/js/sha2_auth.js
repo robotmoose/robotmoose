44e90d838e2ba01bbdc14ab5c7e819ee0d674c32
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-03-28 11:20:40 -0800

    Make superstar read from ../www, the main web directory. This allows local testing, except for the code editor.

---
+++ superstar/web_old/js/sha2_auth.js
@@ -0,0 +1,439 @@
+/*
+The main function here is getAuthCode, at the end, which 
+returns an alphanumeric authentication code based on the 
+hash of the message and a shared secret password.
+
+If you know the password, you can sign messages, but
+the password never crosses the network, only the hash.
+
+We use the SHA-256 hash implementations from CryptoJS:
+	https://crypto-js.googlecode.com/files/CryptoJS%20v3.1.2.zip
+
+Assembled by Dr. Orion Lawlor, lawlor@alaska.edu, 2014-11-17 (Public Domain)
+*/
+
+/* CryptoJS v3.1.2 core.js
+code.google.com/p/crypto-js
+(c) 2009-2013 by Jeff Mott. All rights reserved.
+code.google.com/p/crypto-js/wiki/License
+*/
+var CryptoJS = (function (Math, undefined) {
+    var C = {};
+    var C_lib = C.lib = {};
+    var Base = C_lib.Base = (function () {
+        function F() {}
+        return {
+            extend: function (overrides) {
+                F.prototype = this;
+                var subtype = new F();
+                if (overrides) {
+                    subtype.mixIn(overrides);
+                }
+                if (!subtype.hasOwnProperty('init')) {
+                    subtype.init = function () {
+                        subtype.$super.init.apply(this, arguments);
+                    };
+                }
+                subtype.init.prototype = subtype;
+                subtype.$super = this;
+                return subtype;
+            },
+            create: function () {
+                var instance = this.extend();
+                instance.init.apply(instance, arguments);
+                return instance;
+            },
+            init: function () {
+            },
+            mixIn: function (properties) {
+                for (var propertyName in properties) {
+                    if (properties.hasOwnProperty(propertyName)) {
+                        this[propertyName] = properties[propertyName];
+                    }
+                }
+                if (properties.hasOwnProperty('toString')) {
+                    this.toString = properties.toString;
+                }
+            },
+            clone: function () {
+                return this.init.prototype.extend(this);
+            }
+        };
+    }());
+    var WordArray = C_lib.WordArray = Base.extend({
+        init: function (words, sigBytes) {
+            words = this.words = words || [];
+            if (sigBytes != undefined) {
+                this.sigBytes = sigBytes;
+            } else {
+                this.sigBytes = words.length * 4;
+            }
+        },
+        toString: function (encoder) {
+            return (encoder || Hex).stringify(this);
+        },
+        concat: function (wordArray) {
+            var thisWords = this.words;
+            var thatWords = wordArray.words;
+            var thisSigBytes = this.sigBytes;
+            var thatSigBytes = wordArray.sigBytes;
+            this.clamp();
+            if (thisSigBytes % 4) {
+                for (var i = 0; i < thatSigBytes; i++) {
+                    var thatByte = (thatWords[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+                    thisWords[(thisSigBytes + i) >>> 2] |= thatByte << (24 - ((thisSigBytes + i) % 4) * 8);
+                }
+            } else if (thatWords.length > 0xffff) {
+                for (var i = 0; i < thatSigBytes; i += 4) {
+                    thisWords[(thisSigBytes + i) >>> 2] = thatWords[i >>> 2];
+                }
+            } else {
+                thisWords.push.apply(thisWords, thatWords);
+            }
+            this.sigBytes += thatSigBytes;
+            return this;
+        },
+        clamp: function () {
+            var words = this.words;
+            var sigBytes = this.sigBytes;
+            words[sigBytes >>> 2] &= 0xffffffff << (32 - (sigBytes % 4) * 8);
+            words.length = Math.ceil(sigBytes / 4);
+        },
+        clone: function () {
+            var clone = Base.clone.call(this);
+            clone.words = this.words.slice(0);
+            return clone;
+        },
+        random: function (nBytes) {
+            var words = [];
+            for (var i = 0; i < nBytes; i += 4) {
+                words.push((Math.random() * 0x100000000) | 0);
+            }
+            return new WordArray.init(words, nBytes);
+        }
+    });
+    var C_enc = C.enc = {};
+    var Hex = C_enc.Hex = {
+        stringify: function (wordArray) {
+            var words = wordArray.words;
+            var sigBytes = wordArray.sigBytes;
+            var hexChars = [];
+            for (var i = 0; i < sigBytes; i++) {
+                var bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+                hexChars.push((bite >>> 4).toString(16));
+                hexChars.push((bite & 0x0f).toString(16));
+            }
+            return hexChars.join('');
+        },
+        parse: function (hexStr) {
+            var hexStrLength = hexStr.length;
+            var words = [];
+            for (var i = 0; i < hexStrLength; i += 2) {
+                words[i >>> 3] |= parseInt(hexStr.substr(i, 2), 16) << (24 - (i % 8) * 4);
+            }
+            return new WordArray.init(words, hexStrLength / 2);
+        }
+    };
+    var Latin1 = C_enc.Latin1 = {
+        stringify: function (wordArray) {
+            var words = wordArray.words;
+            var sigBytes = wordArray.sigBytes;
+            var latin1Chars = [];
+            for (var i = 0; i < sigBytes; i++) {
+                var bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+                latin1Chars.push(String.fromCharCode(bite));
+            }
+            return latin1Chars.join('');
+        },
+        parse: function (latin1Str) {
+            var latin1StrLength = latin1Str.length;
+            var words = [];
+            for (var i = 0; i < latin1StrLength; i++) {
+                words[i >>> 2] |= (latin1Str.charCodeAt(i) & 0xff) << (24 - (i % 4) * 8);
+            }
+            return new WordArray.init(words, latin1StrLength);
+        }
+    };
+    var Utf8 = C_enc.Utf8 = {
+        stringify: function (wordArray) {
+            try {
+                return decodeURIComponent(escape(Latin1.stringify(wordArray)));
+            } catch (e) {
+                throw new Error('Malformed UTF-8 data');
+            }
+        },
+        parse: function (utf8Str) {
+            return Latin1.parse(unescape(encodeURIComponent(utf8Str)));
+        }
+    };
+    var BufferedBlockAlgorithm = C_lib.BufferedBlockAlgorithm = Base.extend({
+        reset: function () {
+            this._data = new WordArray.init();
+            this._nDataBytes = 0;
+        },
+        _append: function (data) {
+            if (typeof data == 'string') {
+                data = Utf8.parse(data);
+            }
+            this._data.concat(data);
+            this._nDataBytes += data.sigBytes;
+        },
+        _process: function (doFlush) {
+            var data = this._data;
+            var dataWords = data.words;
+            var dataSigBytes = data.sigBytes;
+            var blockSize = this.blockSize;
+            var blockSizeBytes = blockSize * 4;
+            var nBlocksReady = dataSigBytes / blockSizeBytes;
+            if (doFlush) {
+                nBlocksReady = Math.ceil(nBlocksReady);
+            } else {
+                nBlocksReady = Math.max((nBlocksReady | 0) - this._minBufferSize, 0);
+            }
+            var nWordsReady = nBlocksReady * blockSize;
+            var nBytesReady = Math.min(nWordsReady * 4, dataSigBytes);
+            if (nWordsReady) {
+                for (var offset = 0; offset < nWordsReady; offset += blockSize) {
+                    this._doProcessBlock(dataWords, offset);
+                }
+                var processedWords = dataWords.splice(0, nWordsReady);
+                data.sigBytes -= nBytesReady;
+            }
+            return new WordArray.init(processedWords, nBytesReady);
+        },
+        clone: function () {
+            var clone = Base.clone.call(this);
+            clone._data = this._data.clone();
+            return clone;
+        },
+        _minBufferSize: 0
+    });
+    var Hasher = C_lib.Hasher = BufferedBlockAlgorithm.extend({
+        cfg: Base.extend(),
+        init: function (cfg) {
+            this.cfg = this.cfg.extend(cfg);
+            this.reset();
+        },
+        reset: function () {
+            BufferedBlockAlgorithm.reset.call(this);
+            this._doReset();
+        },
+        update: function (messageUpdate) {
+            this._append(messageUpdate);
+            this._process();
+            return this;
+        },
+        finalize: function (messageUpdate) {
+            if (messageUpdate) {
+                this._append(messageUpdate);
+            }
+            var hash = this._doFinalize();
+            return hash;
+        },
+        blockSize: 512/32,
+        _createHelper: function (hasher) {
+            return function (message, cfg) {
+                return new hasher.init(cfg).finalize(message);
+            };
+        },
+        _createHmacHelper: function (hasher) {
+            return function (message, key) {
+                return new C_algo.HMAC.init(hasher, key).finalize(message);
+            };
+        }
+    });
+    var C_algo = C.algo = {};
+    return C;
+}(Math));
+/* CryptoJS v3.1.2 x64-core.js
+code.google.com/p/crypto-js
+(c) 2009-2013 by Jeff Mott. All rights reserved.
+code.google.com/p/crypto-js/wiki/License
+*/
+(function (undefined) {
+    var C = CryptoJS;
+    var C_lib = C.lib;
+    var Base = C_lib.Base;
+    var X32WordArray = C_lib.WordArray;
+    var C_x64 = C.x64 = {};
+    var X64Word = C_x64.Word = Base.extend({
+        init: function (high, low) {
+            this.high = high;
+            this.low = low;
+        }
+    });
+    var X64WordArray = C_x64.WordArray = Base.extend({
+        init: function (words, sigBytes) {
+            words = this.words = words || [];
+            if (sigBytes != undefined) {
+                this.sigBytes = sigBytes;
+            } else {
+                this.sigBytes = words.length * 8;
+            }
+        },
+        toX32: function () {
+            var x64Words = this.words;
+            var x64WordsLength = x64Words.length;
+            var x32Words = [];
+            for (var i = 0; i < x64WordsLength; i++) {
+                var x64Word = x64Words[i];
+                x32Words.push(x64Word.high);
+                x32Words.push(x64Word.low);
+            }
+            return X32WordArray.create(x32Words, this.sigBytes);
+        },
+        clone: function () {
+            var clone = Base.clone.call(this);
+            var words = clone.words = this.words.slice(0);
+            var wordsLength = words.length;
+            for (var i = 0; i < wordsLength; i++) {
+                words[i] = words[i].clone();
+            }
+            return clone;
+        }
+    });
+}());
+/* CryptoJS v3.1.2 sha256.js
+code.google.com/p/crypto-js
+(c) 2009-2013 by Jeff Mott. All rights reserved.
+code.google.com/p/crypto-js/wiki/License
+*/
+(function (Math) {
+    var C = CryptoJS;
+    var C_lib = C.lib;
+    var WordArray = C_lib.WordArray;
+    var Hasher = C_lib.Hasher;
+    var C_algo = C.algo;
+    var H = [];
+    var K = [];
+    (function () {
+        function isPrime(n) {
+            var sqrtN = Math.sqrt(n);
+            for (var factor = 2; factor <= sqrtN; factor++) {
+                if (!(n % factor)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        function getFractionalBits(n) {
+            return ((n - (n | 0)) * 0x100000000) | 0;
+        }
+        var n = 2;
+        var nPrime = 0;
+        while (nPrime < 64) {
+            if (isPrime(n)) {
+                if (nPrime < 8) {
+                    H[nPrime] = getFractionalBits(Math.pow(n, 1 / 2));
+                }
+                K[nPrime] = getFractionalBits(Math.pow(n, 1 / 3));
+                nPrime++;
+            }
+            n++;
+        }
+    }());
+    var W = [];
+    var SHA256 = C_algo.SHA256 = Hasher.extend({
+        _doReset: function () {
+            this._hash = new WordArray.init(H.slice(0));
+        },
+        _doProcessBlock: function (M, offset) {
+            var H = this._hash.words;
+            var a = H[0];
+            var b = H[1];
+            var c = H[2];
+            var d = H[3];
+            var e = H[4];
+            var f = H[5];
+            var g = H[6];
+            var h = H[7];
+            for (var i = 0; i < 64; i++) {
+                if (i < 16) {
+                    W[i] = M[offset + i] | 0;
+                } else {
+                    var gamma0x = W[i - 15];
+                    var gamma0  = ((gamma0x << 25) | (gamma0x >>> 7))  ^
+                                  ((gamma0x << 14) | (gamma0x >>> 18)) ^
+                                   (gamma0x >>> 3);
+                    var gamma1x = W[i - 2];
+                    var gamma1  = ((gamma1x << 15) | (gamma1x >>> 17)) ^
+                                  ((gamma1x << 13) | (gamma1x >>> 19)) ^
+                                   (gamma1x >>> 10);
+                    W[i] = gamma0 + W[i - 7] + gamma1 + W[i - 16];
+                }
+                var ch  = (e & f) ^ (~e & g);
+                var maj = (a & b) ^ (a & c) ^ (b & c);
+                var sigma0 = ((a << 30) | (a >>> 2)) ^ ((a << 19) | (a >>> 13)) ^ ((a << 10) | (a >>> 22));
+                var sigma1 = ((e << 26) | (e >>> 6)) ^ ((e << 21) | (e >>> 11)) ^ ((e << 7)  | (e >>> 25));
+                var t1 = h + sigma1 + ch + K[i] + W[i];
+                var t2 = sigma0 + maj;
+                h = g;
+                g = f;
+                f = e;
+                e = (d + t1) | 0;
+                d = c;
+                c = b;
+                b = a;
+                a = (t1 + t2) | 0;
+            }
+            H[0] = (H[0] + a) | 0;
+            H[1] = (H[1] + b) | 0;
+            H[2] = (H[2] + c) | 0;
+            H[3] = (H[3] + d) | 0;
+            H[4] = (H[4] + e) | 0;
+            H[5] = (H[5] + f) | 0;
+            H[6] = (H[6] + g) | 0;
+            H[7] = (H[7] + h) | 0;
+        },
+        _doFinalize: function () {
+            var data = this._data;
+            var dataWords = data.words;
+            var nBitsTotal = this._nDataBytes * 8;
+            var nBitsLeft = data.sigBytes * 8;
+            dataWords[nBitsLeft >>> 5] |= 0x80 << (24 - nBitsLeft % 32);
+            dataWords[(((nBitsLeft + 64) >>> 9) << 4) + 14] = Math.floor(nBitsTotal / 0x100000000);
+            dataWords[(((nBitsLeft + 64) >>> 9) << 4) + 15] = nBitsTotal;
+            data.sigBytes = dataWords.length * 4;
+            this._process();
+            return this._hash;
+        },
+        clone: function () {
+            var clone = Hasher.clone.call(this);
+            clone._hash = this._hash.clone();
+            return clone;
+        }
+    });
+    C.SHA256 = Hasher._createHelper(SHA256);
+    C.HmacSHA256 = Hasher._createHmacHelper(SHA256);
+}(Math));
+
+/* 
+Finally, this is the real authentication generation code.
+	Password is the shared secret
+	Path is the full path to the object accessed
+	Message is the new value to authenticate
+	seq is a sequence number (base-10 integer) used to prevent replay attacks.
+The return value is an alphanumeric version of the hash.
+*/
+function getAuthCode(password,path,message,seq) {
+	var hash=CryptoJS.SHA256(password+":"+path+":"+message+":"+seq).toString(); // LONG hex output
+	
+	// Auth code character set, alphanumeric but skipping similar letters "1IlO0"
+	//   This MUST match the same list in the C++ code!
+	var table="ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
+	var lastcopy=Math.floor((1<<16)/table.length)*table.length; // 16 bit chunks
+	
+	// Make auth code by folding 16-bit chunks of the hash together.
+	//   The resulting collisions mean lots of hashes fit the same password.
+	var auth="";
+	var debug="";
+	for (var i=0;i+4<=hash.length;i+=4) {
+		var n=parseInt(hash.slice(i,i+4),16); // eat 4 hex digit chunk
+		if (n<lastcopy) auth += table.charAt(n%table.length);
+		// else skip this part of the key (avoids bias toward low letters)
+		debug+="<br> "+n+" "+auth;
+	}
+	// return hash + debug + " -> " + auth;
+	return auth;
+}
+
