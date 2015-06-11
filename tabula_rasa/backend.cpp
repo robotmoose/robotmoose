e6f01b3358b808a3c35574374bef4d229179cb0d
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-06-10 16:26:49 -0800

    Tuned roomba wheelbase.  It's quite close on hard surface, but on carpet turning left and right differ by about 1%.

--- tabula_rasa/backend.cpp
+++ tabula_rasa/backend.cpp
@@ -440,7 +440,7 @@ void robot_backend::setup_devices(std::string robot_config)
 				
 				sensors.push_back(new wheel_encoders<uint16_t>(location,
 					0.0004444, // wheel travel distance (m) per encoder count
-					0.23 // roomba's wheelbase
+					0.235 // roomba's wheelbase (lower effective wheelbase on carpet)
 					));
 				
 				for (int i=0;i<4;i++)
