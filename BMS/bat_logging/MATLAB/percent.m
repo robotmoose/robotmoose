d3c9305d6ae76baac3ee7f243d8b07bc77ec61b0
Clayton Auld <clayauld@gmail.com>
2015-06-10 18:31:21 -0800

    Battery percentage testing updated.

--- BMS/bat_logging/MATLAB/percent.m
+++ BMS/bat_logging/MATLAB/percent.m
@@ -3,24 +3,40 @@
 
 clear;clc;
 
-time=dlmread('batterytest3.csv',',','A3..A756')/3600;
-Cell0=dlmread('batterytest4.csv',',','B3..B756');
-Cell1=dlmread('batterytest4.csv',',','C3..C756');
-Cell2=dlmread('batterytest4.csv',',','D3..D756');
-Percent=dlmread('batterytest4.csv',',','E3..E756');
+time=dlmread('batterytest5.csv',',','A2..A736')/3600;
+Cell0=dlmread('batterytest5.csv',',','B2..B736');
+Cell1=dlmread('batterytest5.csv',',','C2..C736');
+Cell2=dlmread('batterytest5.csv',',','D2..D736');
+Percent=dlmread('batterytest5.csv',',','E2..E736');
 
 figure(1)
 plot(time,Percent)
 xlabel('Time (hours)');
 ylabel('Percent');
+title('Battery Percentage vs Time')
 
 figure(2)
 plot(time,Cell0,time,Cell1,time,Cell2);
 xlabel('Time (hours)');
 ylabel('Cell Voltage (V)');
+legend('Cell 0','Cell 1','Cell 2');
+title('Cell Voltage vs Time')
 
 charge=Cell0+Cell1+Cell2;
 figure(3)
 plot(time,charge)
 xlabel('Time (hours)');
 ylabel('Battery Voltage (V)');
+title('Battery Voltage vs Time');
+
+figure(4)
+subplot(2,1,1)
+plot(time,charge)
+xlabel('Time (hours)');
+ylabel('Battery Voltage (V)');
+title('Battery Voltage vs Time');
+subplot(2,1,2)
+plot(time,Percent)
+xlabel('Time (hours)');
+ylabel('Percent');
+title('Battery Percentage vs Time')
