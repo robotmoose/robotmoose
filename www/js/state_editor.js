18ffc24b0b40036e01c59da21558902f574772fa
Arsh Chauhan <achauhan@alaska.edu>
2015-06-23 22:08:04 -0800

    Got table creation and roow addition working. Creates correct input types. Added buttons (Submit code, save to disk, load from disk. Added skeleton function to collct data.

--- www/js/state_editor.js
+++ www/js/state_editor.js
@@ -1,4 +1,13 @@
 
+/*
+ * Dynamically create a HTML table with javascript
+ * Users can append new rows to the table, each row has 2 columns
+ * Created for the ITEST project at the Univesity of Alaska, Fairbanks (2015)
+ * 
+ * Goal: Write control code in a web-browser and have it execute on a Create2
+ * 
+ * Public Domain 
+ */
 
 
 function state_editor_t(div)
@@ -10,18 +19,42 @@ function state_editor_t(div)
 	this.table = document.createElement("table");
 		this.table.className = "table table-bordered";
 		this.table.style.padding = "100px;"; // FIXME: Add padding 
+		this.table.id = "states_table";
 	this.thead = document.createElement("thead");
 	this.trHeadings = document.createElement("tr");
 	this.trHeadings.state = document.createElement("th");
 	this.trHeadings.state.innerHTML = "State Name";
 	this.trHeadings.code = document.createElement("th");
 	this.trHeadings.code.innerHTML = "Code";
+	var myself = this;
 	this.add_row = document.createElement("input");
 		this.add_row.type = "button";
 		this.add_row.value = "Add State";
 		this.add_row.className = "btn btn-primary";
-		this.add_row.onclick = function(){this.addRow();}; // FIXME: Throws this.addRow is not a function 
+		this.add_row.onclick = function(){myself.addRow(myself.table);};
 	
+	
+	this.submit_code = document.createElement("input");
+	this.submit_code.type = "button";
+		this.submit_code.value = "Submit Code";
+		this.submit_code.className = "btn btn-primary";
+	    this.submit_code.onclick = function(){console.log("Submit button pressed");myself.collectData("send");}; // TODO: Implement code submission 
+		this.submit_code.style.marginLeft = "1%";
+	
+	this.save_data = document.createElement("input");
+	this.save_data.type = "button";
+		this.save_data.value = "Save to disk";
+		this.save_data.className = "btn btn-primary";
+	    this.save_data.onclick = function(){console.log("Save button pressed");myself.collectData("save");}; // TODO: Implement saving to disk
+		this.save_data.style.marginLeft = "1%";
+	this.tbody = document.createElement("tbody");
+	
+	this.load_data = document.createElement("input");
+	this.load_data.type = "button";
+		this.load_data.value = "Load from disk";
+		this.load_data.className = "btn btn-primary";
+	    this.load_data.onclick = function(){console.log("Load button pressed");};//myself.collectData("save");}; // TODO: Implement loading from disk
+		this.load_data.style.marginLeft = "1%";
 	this.tbody = document.createElement("tbody");
 	this.div.appendChild(this.table);
 	
@@ -31,23 +64,55 @@ function state_editor_t(div)
 	this.trHeadings.appendChild(this.trHeadings.state);
 	this.trHeadings.appendChild(this.trHeadings.code);
 	this.div.appendChild(this.add_row);
-	
-	
-	
+	this.div.appendChild(this.submit_code);
+	this.div.appendChild(this.save_data);
+	this.div.appendChild(this.load_data);
 	
 }
 
-state_editor_t.prototype.addRow = function()
+state_editor_t.prototype.addRow = function(table)
 {
 	var myself = this;
-	var rows = myself.row.length;
-	var table = myself.table;
+	var rows = document.getElementById("states_table").rows.length;
+	console.log("# of rows :"+rows);
+	//var rows = table.Rows.length;
 	
-	var row = table.insertRow(rows+1);
-	var state_name = row.insertCell(0);
-	state_name.type = "text";
-	var code = row.insertCell(1);
-	code.type = "textarea";
+	var row = table.insertRow(-1);
+	var cell1 = row.insertCell(0);
+	cell1.id = "state_cell";
+	document.getElementById("state_cell").style.width = '1%';
+	var state = document.createElement('input');
+	state.setAttribute('type','text');
+	state.setAttribute('value',"");
+	var cell2 = row.insertCell(1);
+	cell2.id = "code_cell";
+	document.getElementById("code_cell").style.width = '1%';
+	var code = document.createElement('textarea');
+	
+	cell1.appendChild(state);
+	cell2.appendChild(code);
+
+}
+
+/*Walk the table DOM to collect all the data.
+ * Check the passed parameter to see if the data has to be sent to superstar or saved to disk 
+*/
+
+state_editor_t.prototype.collectData = function(action)
+{
+	var table = document.getElementById("states_table");
+	console.log("In collectData()")
+	for (var i = 0,row ; row = table.rows[i]; i++)
+	{
+		{
+			
+		}
+		for(var j = 0,cell, cell = row.cells[i]; j++;)
+		{
+		}
+	}
+	console.log("action requested :"+ action);
+	
 }
 
 
