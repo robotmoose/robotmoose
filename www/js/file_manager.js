34c11f5fb76082586134f0c7980c28415b2b0839
Your Name <you@example.com>
2015-06-26 16:47:18 -0800

    Added file manager style to js...

--- www/js/file_manager.js
+++ www/js/file_manager.js
@@ -46,6 +46,7 @@ function load_dependencies()
 	load_js("/js/jquery/jquery.min.js");
 	load_js("/js/bootstrap/bootstrap.min.js");
 	load_link("/css/bootstrap.min.css");
+	load_style(".btn-file {position: relative;overflow: hidden;}\n.btn-file input[type=file] {position: absolute;top: 0;right: 0;min-width: 100%;min-height: 100%;font-size: 100px;text-align: right;filter: alpha(opacity=0);opacity: 0;background: red;cursor: inherit;display: block;}\ninput[readonly] {background-color: white !important;cursor: text !important;}");
 };
 
 (function(){load_dependencies()})();
