c398df5d35dde7f5db8e5f3dedfb3d04990fb346
Your Name <you@example.com>
2015-07-09 14:31:59 -0800

    Added glyphs to menu bar.

--- www/js/robot_menu.js
+++ www/js/robot_menu.js
@@ -33,19 +33,34 @@ function robot_menu_t(div)
 	(
 		"Robot",
 		null,
+		"glyphicon glyphicon-cog",
 		{
-			"New":function(){alert("New!");},
-			"Open":function(){alert("Open!");}
+			"Connect":
+			{
+				onclick:function(){alert("Connect!");},
+				glyph:"glyphicon glyphicon-off"
+			},
+			"Make a Copy":
+			{
+				onclick:function(){alert("Copy!");},
+				glyph:"glyphicon glyphicon-duplicate"
+			}
 		}
 	);
 }
 
-robot_menu_t.prototype.create_button=function(name,onclick,options)
+robot_menu_t.prototype.create_button=function(name,onclick,glyph,options)
 {
 	var drop={};
 	this.drops[name]={};
 	this.drops[name].li=document.createElement("li");
 	this.drops[name].a=document.createElement("a");
+	this.drops[name].glyph=document.createElement("span");
+	this.drops[name].text=document.createTextNode(" "+name+" ");
+
+	if(!glyph||glyph.length==0)
+		this.drops[name].text=document.createTextNode(name+" ");
+
 	this.drops[name].span=document.createElement("span");
 	this.drops[name].ul=document.createElement("ul");
 	this.drops[name].options={};
@@ -54,13 +69,17 @@ robot_menu_t.prototype.create_button=function(name,onclick,options)
 	this.menu.ul.appendChild(this.drops[name].li);
 
 	this.drops[name].a.href="javascript:void(0);";
-	this.drops[name].a.innerHTML=name+" ";
 	this.drops[name].a.className="dropdown-toggle";
 	this.drops[name].a.setAttribute("data-toggle","dropdown");
 	this.drops[name].a.setAttribute("role","button");
 	this.drops[name].a.setAttribute("aria-haspopup","true");
 	this.drops[name].a.setAttribute("aria-expanded","false");
 	this.drops[name].li.appendChild(this.drops[name].a);
+
+	this.drops[name].glyph.className=glyph;
+	this.drops[name].a.appendChild(this.drops[name].glyph);
+
+	this.drops[name].a.appendChild(this.drops[name].text);
 
 	this.drops[name].span.className="caret";
 	this.drops[name].a.appendChild(this.drops[name].span);
@@ -73,12 +92,18 @@ robot_menu_t.prototype.create_button=function(name,onclick,options)
 		this.drops[name].options[key]={};
 		this.drops[name].options[key].li=document.createElement("li");
 		this.drops[name].options[key].a=document.createElement("a");
+		this.drops[name].options[key].span=document.createElement("span");
+		this.drops[name].options[key].text=document.createTextNode(" "+key);
 
 		this.drops[name].ul.appendChild(this.drops[name].options[key].li);
 
 		this.drops[name].options[key].a.href="javascript:void(0);";
-		this.drops[name].options[key].a.innerHTML=key;
-		this.drops[name].options[key].a.onclick=options[key];
+		this.drops[name].options[key].a.onclick=options[key].onclick;
 		this.drops[name].options[key].li.appendChild(this.drops[name].options[key].a);
+
+		this.drops[name].options[key].span.className=options[key].glyph;
+		this.drops[name].options[key].a.appendChild(this.drops[name].options[key].span);
+
+		this.drops[name].options[key].a.appendChild(this.drops[name].options[key].text);
 	}
 }
