7f3273da2a2a1103a8e620cb6188095e14b07163
Clayton Auld <clayauld@gmail.com>
2015-08-07 16:51:58 -0800

    Changed max_working voltage in BMS code.

--- BMS/firmware/firmware.ino
+++ BMS/firmware/firmware.ino
@@ -35,7 +35,7 @@
 // Charge function parameters
 
 #define ABS_max           4.2  // Max Cell voltage: DO NOT CHARGE HIGHER
-#define max_working       4.111  // Discharge cells higher than this value
+#define max_working       4.001  // Discharge cells higher than this value
 #define low_cell_working  3.500  // Warning Voltage
 #define ABS_min           3.200  //E-Stop or risk damage
 
