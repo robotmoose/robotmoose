1f3e8dc4d416caccd300714725a8916d4472c4fe
Annie <annietupek@gmail.com>
2015-08-28 16:43:19 -0800

    added dropdown

--- www/js/video_webrtc.js
+++ www/js/video_webrtc.js
@@ -7,15 +7,31 @@ function video_webrtc_t(div)
 	this.div.style.width="100%";
 	this.div.style.height="100%";
 	this.div.style.overflow="hidden";
+
+	var myself=this;
+	this.robot=null;
+
+	this.select=document.createElement("select");
+	this.div.appendChild(this.select);
+	this.select.className="form-control";
+	this.select.onchange=function(){myself.download(null, this.options[this.selectedIndex].video_link);};
+
+	this.create_option("webRTC","http://www.robotmoose.com/webrtc/");
+	this.create_option("gruveo","https://www.gruveo.com/embed/");
 }
 
-video_webrtc_t.prototype.download=function(robot)
+video_webrtc_t.prototype.download=function(robot, link)
 {
+	if(robot != null)
+	{
+		this.robot = robot;
+	}
+
 	if(this.frame)
-		this.element.removeChild(this.frame);
+	this.element.removeChild(this.frame);
 
 	this.frame=document.createElement("iframe");
-	this.frame.src="http://test.robotmoose.com/webrtc/?robot="+robot.name;
+	this.frame.src=link + "?robot=" + this.robot.name;
 	this.frame.style.width="100%";
 	this.frame.style.height="100%";
 	this.frame.style.border="none";
@@ -24,4 +40,15 @@ video_webrtc_t.prototype.download=function(robot)
 	this.frame.style.overflow="hidden";
 	this.frame.style.pointerEvents="none";
 	this.div.appendChild(this.frame);
+}
+
+video_webrtc_t.prototype.create_option=function(name, link)
+{
+	var option = document.createElement("option");
+
+	option.video_link=link;
+	option.text = name;
+
+	this.select.appendChild(option);
+
 }
