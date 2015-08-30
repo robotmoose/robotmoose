87fab7b149837014f0d8242baaeb8c96037d6407
Your Name <you@example.com>
2015-08-30 12:46:35 -0800

    Frame is null...

--- www/js/video_widget.js
+++ www/js/video_widget.js
@@ -58,9 +58,9 @@ video_widget_t.prototype.download=function(robot,link)
 			this.frame.style.pointerEvents="all";
 		else
 			this.frame.style.pointerEvents="none";
-	}
 
-	this.div.appendChild(this.frame);
+		this.div.appendChild(this.frame);
+	}
 }
 
 video_widget_t.prototype.create_option=function(name,link)
