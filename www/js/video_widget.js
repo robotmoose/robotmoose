5994478691bf2bd4b93a73e8630f9a828741682e
Your Name <you@example.com>
2015-08-30 12:43:17 -0800

    Added an empty option to the video width (renamed as well).

--- www/js/video_widget.js
+++ www/js/video_widget.js
@@ -1,4 +1,4 @@
-function video_webrtc_t(obj)
+function video_widget_t(obj)
 {
 	if(!obj)
 		return null;
@@ -25,39 +25,45 @@ function video_webrtc_t(obj)
 	this.select.onchange=function(){myself.download(null,myself.select.options[this.selectedIndex].video_link);};
 
 	this.default_link="http://robotmoose.com/webrtc/";
-	this.create_option("webRTC",this.default_link);
-	this.create_option("gruveo","https://www.gruveo.com/embed/");
+	this.create_option("None",null);
+	this.create_option("WebRTC",this.default_link);
+	this.create_option("Gruveo","https://www.gruveo.com/embed/");
 }
 
-video_webrtc_t.prototype.download=function(robot,link)
+video_widget_t.prototype.download=function(robot,link)
 {
-	if(robot!=null)
-		this.robot=robot;
-
-	if(!link)
-		link=this.default_link;
-
 	if(this.frame)
+	{
 		this.div.removeChild(this.frame);
+		this.frame=null;
+	}
 
-	this.frame=document.createElement("iframe");
-	this.frame.src=link+"?robot="+this.robot.name+"&doorways=true";
-	this.frame.style.width="100%";
-	this.frame.style.height="100%";
-	this.frame.style.border="none";
-	this.frame.style.padding="0px";
-	this.frame.style.margin="0px";
-	this.frame.style.overflow="hidden";
+	if(robot==null)
+		return;
 
-	if(this.obj.active)
-		this.frame.style.pointerEvents="all";
-	else
-		this.frame.style.pointerEvents="none";
+	this.robot=robot;
+
+	if(link)
+	{
+		this.frame=document.createElement("iframe");
+		this.frame.src=link+"?robot="+this.robot.name+"&doorways=true";
+		this.frame.style.width="100%";
+		this.frame.style.height="100%";
+		this.frame.style.border="none";
+		this.frame.style.padding="0px";
+		this.frame.style.margin="0px";
+		this.frame.style.overflow="hidden";
+
+		if(this.obj.active)
+			this.frame.style.pointerEvents="all";
+		else
+			this.frame.style.pointerEvents="none";
+	}
 
 	this.div.appendChild(this.frame);
 }
 
-video_webrtc_t.prototype.create_option=function(name,link)
+video_widget_t.prototype.create_option=function(name,link)
 {
 	var option=document.createElement("option");
 	option.video_link=link;
@@ -65,10 +71,13 @@ video_webrtc_t.prototype.create_option=function(name,link)
 	this.select.appendChild(option);
 }
 
-video_webrtc_t.prototype.pointer_events=function(value)
+video_widget_t.prototype.pointer_events=function(value)
 {
-	this.frame.style.pointerEvents="none";
+	if(this.frame)
+	{
+		this.frame.style.pointerEvents="none";
 
-	if(value&&!this.obj.resizer.dragging.on)
-		this.frame.style.pointerEvents="all";
+		if(value&&!this.obj.resizer.dragging.on)
+			this.frame.style.pointerEvents="all";
+	}
 }
