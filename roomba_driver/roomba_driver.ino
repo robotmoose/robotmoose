8a44c6d1d0926b506859121423327ff2177c309f
Mike Moss <mrmoss@alaska.edu>
2015-09-28 09:38:48 -0800

    Roomba driving systems tested and actually work now...

--- roomba_driver/roomba_driver.ino
+++ roomba_driver/roomba_driver.ino
@@ -1,25 +1,39 @@
 #include "roomba.h"
 
-roomba_t roomba(Serial);
+roomba_t roomba(Serial3);
 int speed=50;
 
 void setup()
 {
-  Serial.begin(57600);
-  roomba.reset();
-  roomba.set_mode(roomba_t::SAFE);
-
-  roomba.drive(speed,speed);
-  delay(4000);
-  roomba.drive(speed,-speed);
-  delay(6000);
-  roomba.drive(speed,speed);
-  delay(4000);
-  roomba.drive(-speed,speed);
-  delay(6000);
-  roomba.drive(0,0);
+  Serial.begin(115200);
+  Serial3.begin(115200);
+  roomba.setup(2);
+  Serial.println("Roomba Started");
 }
 
 void loop()
 {
+  roomba.update();
+
+  Serial.println("Moving Forward");
+  roomba.drive(speed,speed);
+  delay(4000);
+
+  Serial.println("Turning Right");
+  roomba.drive(speed,-speed);
+  delay(6000);
+
+  Serial.println("Moving Forward");
+  roomba.drive(speed,speed);
+  delay(4000);
+
+  Serial.println("Turning Left");
+  roomba.drive(-speed,speed);
+  delay(6000);
+
+  Serial.println("Stopping");
+  roomba.drive(0,0);
+
+  Serial.println("Safe Mode");
+  roomba.set_mode(roomba_t::SAFE);
 }
--- roomba_driver_serial/roomba_driver_serial.ino
+++ roomba_driver_serial/roomba_driver_serial.ino
@@ -1,34 +1,46 @@
 #include "roomba.h"
 
-roomba_t roomba(Serial1);
+roomba_t roomba(Serial3);
 int speed=50;
 
 void setup()
 {
-  Serial.begin(57600);
-  roomba.reset();
+  Serial.begin(115200);
+  Serial3.begin(115200);
+  roomba.setup(2);
+  Serial.println("Roomba Started");
   roomba.set_mode(roomba_t::SAFE);
 }
 
 void loop()
 {
+  roomba.update();
   int command=Serial.read();
-  
-  switch(command)
+
+  if(command>=0)
   {
-    case 'f':
-      roomba.drive(speed,speed);
-      break;
-    case 'b':
-      roomba.drive(-speed,-speed);
-      break;
-    case 'l':
-      //Make it go left.
-      break;
-    case 'r':
-      //Make it go right.
-      break;
-    default:
-      roomba.drive(0,0);
+    switch(command)
+    {
+      case 'f':
+        Serial.println("Moving Forward");
+        roomba.drive(speed,speed);
+        break;
+      case 'b':
+        Serial.println("Moving Backward");
+        roomba.drive(-speed,-speed);
+        break;
+      case 'l':
+        Serial.println("Turning Left");
+        //Make it go left.
+        break;
+      case 'r':
+        Serial.println("Turning Right");
+        //Make it go right.
+        break;
+      case 's':
+        Serial.println("Stopping");
+        roomba.drive(0,0);
+        break;
+    }
   }
 }
