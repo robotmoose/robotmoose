2a6710fa25aa7a34d2ebcbda3c6fb7d5f3b9cbaf
Dr. Orion Lawlor <lawlor@alaska.edu>
2015-10-19 16:57:38 -0800

    Make interactive_code logs area have correct permissions after update.

--- update_git.sh
+++ update_git.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 cd `dirname $0`
 sudo git pull
-sudo chown -R www-data ../
-sudo chgrp -R itest ../
-sudo chown -R no_priv superstar/db.bak
-sudo chgrp -R no_priv superstar/db.bak
-sudo chown -R no_priv superstar/backups
-sudo chgrp -R no_priv superstar/backups
-sudo chown -R no_priv superstar/logs
-sudo chgrp -R no_priv superstar/logs
+
+sudo chown -R www-data:itest ../
+sudo chown -R no_priv:no_priv superstar/db.bak
+sudo chown -R no_priv:no_priv superstar/backups
+sudo chown -R no_priv:no_priv superstar/logs
+sudo chown -R no_priv:no_priv code_editor/logs
