710aeb8bd2f0d8af7b4cdc065b08103f2a64de94
Ryker Dial <ryker.dial@gmail.com>
2016-06-03 13:22:23 -0800

    Microphone backend now performs FFT on received data.

--- sensors/sound_localize/software/Microphone_Arduino/Microphone_Arduino.ino
+++ sensors/sound_localize/software/Microphone_Arduino/Microphone_Arduino.ino
@@ -1,3 +1,9 @@
+// Arduino Firmware for interfacing with microphone array.
+// NOTE! This code is written specifically for the Arduino Due.
+
+// Ryker Dial
+// UAF ITEST
+
 size_t numSensors = 5; // Five microphone array in pentagon shape.
 uint16_t micData[5][4];
 uint32_t timeRead[5];
@@ -5,15 +11,19 @@ char buff;
 
 // Connect 
 void setup() {
-    Serial.begin(921600, SERIAL_8O2); // odd parity, 2 stop bits
+    //Serial.begin(921600, SERIAL_8O2); // odd parity, 2 stop bits
+    //REG_ADC_MR = (REG_ADC_MR & 0xFFF0FFFF) | 0x00020000;
+    ADC->ADC_MR |= 0x80;  //set free running mode on ADC
+    //SerialUSB.begin(921600, SERIAL_8O2);
+    SerialUSB.begin(9600);
 }
 
 void loop() {
-   // while(true) { // read analog values as fast as possible
             // Send CRLF to synchronize with server
-            Serial.write(10); // CR
-            Serial.write(13); // LF
+            SerialUSB.write(10); // CR
+            SerialUSB.write(13); // LF
             for(int i=0; i<numSensors; ++i) {
+               // micData[i][0] = analogRead(i);
                 // Get 4 samples. Gaussian noise decreases by 1/N^2, where N is the
                 //     number of samples
                 for(int j=0; j<4; ++j) {
@@ -24,22 +34,22 @@ void loop() {
                     micData[i][0] += micData[i][j];
                 }
                 micData[i][0] = micData[i][0]/2;
-                timeRead[i] = micros(); // Note: micros has 4 microsecond resolution.
+               // timeRead[i] = micros(); // Note: micros has 4 microsecond resolution.
             }
+            timeRead[0] = micros();
             // Send over microphone data over serial with time data
-            for(int i=0; i<numSensors; ++i) {
                 for(int j=3; j>=0; --j) { // sizeof(uint32_t) - 1 = 3
                     // write is lower latency than print, just requires more backend parsing.
-                    Serial.write( (uint8_t) (timeRead[i] >> 8*j) );
-                    //Serial.print(timeRead[i]);
-                    //Serial.print(",");
+                    SerialUSB.write( (uint8_t) (timeRead[0] >> 8*j) );
+
                 }
+            for(int i=0; i<numSensors; ++i) {
+                /*for(int j=3; j>=0; --j) { // sizeof(uint32_t) - 1 = 3
+                    // write is lower latency than print, just requires more backend parsing.
+                    SerialUSB.write( (uint8_t) (timeRead[i] >> 8*j) );
+                }*/
                 for(int j=1; j>=0; --j) { // sizeof(uint16_t) - 1 = 1
-                    Serial.write( (uint8_t) (micData[i][0] >> 8*j) );
-                    //Serial.print(micData[i][0]);
-                    //Serial.print(",");
+                    SerialUSB.write( (uint8_t) (micData[i][0] >> 8*j) );
                 }
             }
-            //Serial.println("");
-    //}
 }
--- sensors/sound_localize/software/Sound_Interface.cpp
+++ sensors/sound_localize/software/Sound_Interface.cpp
@@ -4,16 +4,23 @@
 // UAF ITEST
 
 // Date Created: 5/31/2016
-// Last Modified: 6/1/2016
+// Last Modified: 6/3/2016
 
 #include <string>
 #include <cstdint>
 #include <iostream> // for std::cout, std::endl
 #include <vector>
-#include <cstdio> // for std::printf
+#include <deque>
+#include <cstdio> // for std::printf & std::sprintf
+#include <utility> // for std::pair
 #include "serial/serial.h"
+#include <fftw3.h>
+
+#include <fstream>
 
 int main() {
+
+	// ********** Serial Port Setup Begin ********** //
 	// Serial Port Parameters
 	std::string port = "";
 	std::uint32_t baudrate = 921600;
@@ -23,10 +30,14 @@ int main() {
 	serial::stopbits_t stopbits = serial::stopbits_two;
 	serial::flowcontrol_t flowcontrol = serial::flowcontrol_none;
 
-	// Get Arduino port name automatically
+	//Get Arduino port name automatically
 	std::vector<serial::PortInfo> portInfo = serial::list_ports();
 	for(auto it = portInfo.begin(); it != portInfo.end(); ++it) {
-		if((it -> description).find("Arduino") || (it -> description).find("arduino")) {
+		std::cout << it -> description << std::endl;
+		std::cout << it -> port << std::endl;
+		if((it -> description).find("Arduino") 
+			|| (it -> description).find("arduino")
+			|| (it -> description).find("USB2.0-Serial")) { // Generic Nano
 			port = it -> port;
 			break;
 		}
@@ -49,39 +60,125 @@ int main() {
 		return -1;
 	}
 	else std::cout << "Serial port " + port + " is open." << std::endl;
+	// ********** Serial Port Setup End ********** // 
 
+
+	// ********** Data Parsing and Processing Setup Begin ********** // 
 	// Variables and containers needed for parsing the serial data
-	std::size_t num_microphones = 5;
-	std::size_t data_length = num_microphones*(4+2);
-	std::uint8_t buffer[100];
-	std::vector<std::uint32_t> time_micros(num_microphones);
-	std::vector<std::uint16_t> microphone_data(num_microphones);
+	std::size_t num_streams = 5; // Number of data streams = number of microphones
 
+	std::size_t data_length = num_streams*(2)+4; // 2 bytes per microphone + 4 bytes for time
+	std::uint8_t buffer[100];
+	std::uint32_t time_micros;
+	std::vector<std::uint16_t> microphone_data(num_streams);
+
+	std::uint16_t fs = 7800; // ADC Sampling Frequency
+	// Number of samples to use for FFT. FFT resolution is fs/N.
+	std::size_t num_samples_fft = 2048;
+	// Counts up to num_samples_fft, then triggers FFT. Is reset to N/2 so FFTs overlap.
+	std::size_t fft_counter = 0; 
+	
+	// Create a vector of data streams. Each data stream is a deque containing the received
+	//     microphone readings.
+	std::vector<std::deque<std::uint16_t>> data_streams;
+	for(int i=0; i<num_streams; ++i) {
+		data_streams.push_back(std::deque<std::uint16_t>());
+	}
+
+	// Open up files for data logging
+	std::ofstream output_time("output_time.csv", std::ios::out | std::ios::trunc);
+	std::ofstream output_freq("output_freq.csv", std::ios::out | std::ios::trunc);
+	if(!(output_time.is_open()) || !(output_freq.is_open())) {
+		std::cout << "Error: Unable to open log files!" << std::endl;
+		return -1;
+	}
+	char buff[50];
+
+	// Set up mechanisms for FFT
+	std::vector<std::pair<double *, double *>> fft_data;
+
+	for(int i=0; i<num_streams; ++i) {
+		fft_data.push_back(std::make_pair(
+			(double*) fftw_malloc(sizeof(double) * num_samples_fft),
+			(double*) fftw_malloc(sizeof(double) * num_samples_fft)
+		));
+	}
+	//in = (double*) fftw_malloc(sizeof(double) * num_samples_fft);
+	//out = (double*) fftw_malloc(sizeof(double) * num_samples_fft);
+	// Create optimized plan for executing FFT
+	fftw_plan p = fftw_plan_r2r_1d(
+		num_samples_fft, 
+		fft_data[0].first, 
+		fft_data[0].second, 
+		FFTW_R2HC, 
+		FFTW_MEASURE
+	);
+
+	// ********** Data Parsing and Processing Setup End ********** // 
+
+
+	// ********** Main Program Loop ********** //
 	while(true) {
+		// Syncronize by reading CR (10) followed by LF (13)
 		sp.read(buffer,1);
 		if(buffer[0] == 10) {
 			sp.read(buffer,1);
 			if(buffer[0] == 13) { // Synchronized
-				std::cout << "Synchronized" << std::endl;
 				sp.read(buffer, data_length);
+				++fft_counter;
 
-				for(int i=0; i<num_microphones; ++i) {
-					time_micros[i] = 0;
+				// Reconstruct time data
+				time_micros = 0;
+				for(int i=0; i<4; ++i) {
+					time_micros += buffer[i] << 8*(3-i);
+				}
+				//std::printf("Time data: %u\n", time_micros);
+				for(int i=0; i<num_streams; ++i) {
 					microphone_data[i] = 0;
-					// Reconstruct time data
-					for(int j=0; j<4; ++j) {
-						time_micros[i] += buffer[j+i*(4+2)] << 8*(3-j);
-					}
 					// Reconstruct microphone data
 					for(int j=0; j<2; ++j) {
-						microphone_data[i] = buffer[j+4+i*(4+2)] << 8*(1-j);
+						microphone_data[i] += buffer[4+j+i*2] << 8*(1-j);
 					}
-					std::printf("Time data %d: %u\n", i, time_micros[i]);
-					std::printf("Microphone data %d: %u\n", i, microphone_data[i]);
+					// Store data for use with FFT
+					data_streams[i].push_back(microphone_data[i]);
+					if(data_streams[i].size() > num_samples_fft) data_streams[i].pop_front();
+					
+					std::sprintf(buff, "%u,", microphone_data[i]);
+					output_time << buff;
+					//std::printf("Microphone data %d: %u\n", i, microphone_data[i]);
 				}
+				//std::printf("Counter: %lu\n", fft_counter);
+				if(fft_counter >= num_samples_fft) {
+					// Execute the Fourier transforms
+					for(int i=0; i<num_streams; ++i) {
+						for(int j=0; j<num_samples_fft; ++j) {
+							// Copy microphone data to in. This will convert int to double.
+							fft_data[i].first[j] = (double) data_streams[i][j];
+						}
+						//clearfftw_execute(p); // perform the fft
+						fftw_execute_r2r(p, fft_data[i].first, fft_data[i].second);
+						std::printf("FFT #%d performed.\n", i);
+					}
+					// Log the FFT outputs.
+					for(int i=0; i<(num_samples_fft+1)/2; ++i) {
+						for(int j=0; j<num_streams; ++j) {
+							std::sprintf(buff, "%f,", fft_data[j].second[i]);
+							output_freq << buff;			
+						}
+						output_freq << "\n";
+					}
+					fft_counter = num_samples_fft/2;
+				}
+				output_time << "\n";
 			}
 		}
 	}
+	// Clean Up
 	sp.close();
+	fftw_destroy_plan(p);
+	for(int i=0; i<num_streams; ++i) {
+		fftw_free(fft_data[i].first);
+		fftw_free(fft_data[i].second);
+	}
 	return 0;
 }
