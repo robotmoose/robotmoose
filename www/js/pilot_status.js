3197249a00a2aa72118de16442f9bfbfec523d85
Arsh Chauhan <achauhan@alaska.edu>
2016-06-03 19:14:36 -0800

    pilot status class for frontend

---
+++ www/js/pilot_status.js
@@ -0,0 +1,37 @@
+/*
+	Arsh Chauhan
+	06/03/2016
+	Public Domain
+*/
+
+/*
+	pilot_status.js
+	Class to handle pilot status. 
+	Increments a counter as long as a pilot is connected
+	Used for backend pilot detetcion   
+*/
+
+//TO DO: Detect multiple pilots.  
+
+function pilot_status_t(ui)
+{
+	if(!ui || !ui.robot)
+		return null;
+
+	this.current_heartbeat = 0;
+	this.ui = ui;
+	this.path = "pilotHeartbeat";
+	var _this = this;
+
+	setInterval(function(){_this.upload();},1000);
+
+}
+
+pilot_status_t.prototype.upload=function()
+{
+	this.current_heartbeat++
+	if(this.current_heartbeat > 255)
+		this.current_heartbeat = 0;
+	console.log("Pilot Heartbeat: " + this.current_heartbeat);
+	superstar_set(this.ui.robot,this.path,{"pilotHeartbeat":this.current_heartbeat});
+}
