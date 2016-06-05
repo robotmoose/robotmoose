efd75052d47b7be324ec06bac70ad0ba92ceb454
Mike Moss <mrmoss@alaska.edu>
2016-06-05 12:36:35 -0800

    Minor ui tweaking...

--- tabula_rasa/chromeapp/js/src/gui.js
+++ tabula_rasa/chromeapp/js/src/gui.js
@@ -73,7 +73,7 @@ function gui_t(div)
 		[
 			{type:'left',resizable:true,content:this.gruveo_div,size:"65%"},
 			{type:'main',resizable:true,content:this.name.el,},
-			{type:'preview',resizable:true,content:this.status_viewer.el,size:"85%"}
+			{type:'preview',resizable:true,content:this.status_viewer.el,size:"80%"}
 		]
 	});
 }
