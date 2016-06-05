cd92ce9089c76f93ad8c7fdb6fc76b481b820641
Mike Moss <mrmoss@alaska.edu>
2016-06-05 12:37:40 -0800

    Minor ui tweaking...

--- tabula_rasa/chromeapp/js/src/gui.js
+++ tabula_rasa/chromeapp/js/src/gui.js
@@ -71,7 +71,7 @@ function gui_t(div)
 		name:'app_layout',
 		panels:
 		[
-			{type:'left',resizable:true,content:this.gruveo_div,size:"65%"},
+			{type:'left',resizable:true,content:this.gruveo_div,size:"60%"},
 			{type:'main',resizable:true,content:this.name.el,},
 			{type:'preview',resizable:true,content:this.status_viewer.el,size:"80%"}
 		]
