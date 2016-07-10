8ac1e2b51da79ecfe14a8e3d9a2c0199d84da472
Mike Moss <mrmoss@alaska.edu>
2016-07-09 16:49:10 -0800

    Authentication now reads from the auth file and has recursive style auth codes like superstar v1. Closes #99.

--- superstarv2/src/superstar.cpp
+++ superstarv2/src/superstar.cpp
@@ -6,6 +6,7 @@
 #include "superstar.hpp"
 
 #include "auth.hpp"
+#include <fstream>
 #include <sstream>
 #include "string_util.hpp"
 
@@ -14,16 +15,21 @@
 superstar_t::superstar_t(const std::string& auth_file):auth_file_m(auth_file)
 {}
 
+//Replace all multiple slashes with a single slash (hacky...but it works...)
+static std::string remove_double_slashes(std::string str)
+{
+	size_t times=str.size()/2+1;
+	for(size_t ii=0;ii<times;++ii)
+		str=replace_all(str,"//","/");
+	return str;
+}
+
 //Turn a path into a vector of strings.
 //  "///test//hello//blah/" yields {"test","hello","blah"}.
 std::vector<std::string> superstar_t::pathify(std::string path) const
 {
-	//Remove leading and trailing slashes...
-	path=strip(path,"/");
-
-	//Replace all multiple slashes with a single slash (hacky...but it works...)
-	for(size_t ii=0;ii<path.size()/2+1;++ii)
-		path=replace_all(path,"//","/");
+	//Remove multiple,leading, and trailing slashes...
+	path=remove_double_slashes(strip(path,"/"));
 
 	//Finally tokenize the path...
 	return split(path,"/");
@@ -67,9 +73,10 @@ Json::Value superstar_t::get(const std::string& path) const
 }
 
 //Sets value of the "JSON" object path to val.
-//  Creates full path if path doesn't exist.
-//  If existing key are not objects in the path, they will be afterwards...
-//    So setting /a/b to foo on object {"a":123} would yield {"a":{"b":"foo"}}.
+//  Note, Creates full path if path doesn't exist.
+//  Note, If existing key are not objects in the path,
+//        they will be afterwards... So setting /a/b to foo in
+//        object {"a":123} would yield: {"a":{"b":"foo"}}...
 void superstar_t::set(const std::string& path,const Json::Value& val)
 {
 	//Tokenize path.
@@ -119,7 +126,8 @@ void superstar_t::set(const std::string& path,const Json::Value& val)
 }
 
 //Lists all keys in an object or array.
-// Note, follows javascript standard when dealing with arrays (returns indicies).
+//  Note, follows javascript standard when dealing with arrays (
+//  returns indicies).
 Json::Value superstar_t::sub(const std::string& path) const
 {
 	//Tokenize path.
@@ -180,7 +188,8 @@ Json::Value superstar_t::sub(const std::string& path) const
 
 //Pushes a val into an array located at path and resizes it to len.
 //  Arrays here are treated as queues.
-//  Similar functionality of set when dealing with existing key names in the given path.
+//  Similar functionality of set when dealing with existing key names in
+//  the given path.
 void superstar_t::push(const std::string& path,const Json::Value& val,
 	const Json::Value& len)
 {
@@ -252,13 +261,80 @@ void superstar_t::push(const std::string& path,const Json::Value& val,
 	*obj=new_array;
 }
 
-//Authenticates path with opts with the passed auth object (expects a string or null).
-//  FIXME:  Authentication is hardcoded to 123...
-bool superstar_t::auth_check(const std::string& path,const std::string& opts,
-	const Json::Value& auth)
+//Authenticates path with opts with the passed auth object
+//  Note, expects a string or null in the auth object.
+//  Note, recursive function, passing the original path around in
+//        recursive_path...
+//  Note, PATH IS STRIPPED OF ALL PRECEEDING /'s and ENDING /'s,
+//        AND MULTIPLE /'s ARE CHANGED TO A SINGLE /.
+bool superstar_t::auth_check(std::string path,const std::string& opts,
+	const Json::Value& auth,std::string recursive_path)
 {
+	//Remove multiple,leading, and trailing slashes...
+	path=remove_double_slashes(strip(path,"/"));
+
+	//This is a recursive function, need to pass the path around...
+	if(recursive_path.size()==0)
+		recursive_path=path;
+
+	//Try to open auth file.
+	std::ifstream fstr(auth_file_m);
+
+	//No passwords at all...authenticated...
+	if(!fstr.good())
+		return true;
+
+	//Open auth file and parse passwords in line based format "PATH PASSWORD" (without quotes).
+	bool found=false;
+	std::string pass;
+	while(std::getline(fstr,pass))
+	{
+		//Read whole line into pass...need to separate into path and pass...
+		std::istringstream istr(pass);
+		std::string challenge_path;
+		istr>>challenge_path;
+
+		//Strip beginning and ending slashes to make path checks easier...
+		challenge_path=remove_double_slashes(strip(challenge_path,"/"));
+
+		//Found path, grab password...
+		if(challenge_path==path)
+		{
+			//Empty password...
+			if(!(istr>>pass))
+				pass="";
+			found=true;
+			break;
+		}
+	}
+	fstr.close();
+
+	//No password found...
+	if(!found)
+	{
+		//Remove top level of path.
+		std::string next_path=path;
+		while(next_path.size()>0&&next_path[next_path.size()-1]!='/')
+			next_path=next_path.substr(0,next_path.size()-1);
+
+		//Remove multiple,leading, and trailing slashes...
+		next_path=remove_double_slashes(strip(next_path,"/"));
+
+		//If top level path isn't blank and not the same as the old one, try it's auth.
+		if(next_path!=path)
+			return auth_check(next_path,opts,auth,recursive_path);
+
+		//Else no auth, authenticated.
+		return true;
+	}
+
+	//Blank password is the same as no password.
+	if(pass.size()<=0)
+		return true;
+
+	//Perform HMAC.
 	std::string auth_str="";
 	if(!auth.isNull())
 		auth_str=auth.asString();
-	return (auth_str==to_hex_string(hmac_sha256("123",path+":"+opts)));
+	return (auth_str==to_hex_string(hmac_sha256(pass,recursive_path+":"+opts)));
 }
--- superstarv2/src/superstar.hpp
+++ superstarv2/src/superstar.hpp
@@ -24,23 +24,31 @@ class superstar_t
 		Json::Value get(const std::string& path) const;
 
 		//Sets value of the "JSON" object path to val.
-		//  Creates full path if path doesn't exist.
-		//  If existing key are not objects in the path, they will be afterwards...
-		//    So setting /a/b to foo on object {"a":123} would yield {"a":{"b":"foo"}}.
+		//  Note, Creates full path if path doesn't exist.
+		//  Note, If existing key are not objects in the path,
+		//        they will be afterwards... So setting /a/b to foo in
+		//        object {"a":123} would yield: {"a":{"b":"foo"}}...
 		void set(const std::string& path,const Json::Value& val);
 
 		//Lists all keys in an object or array.
-		// Note, follows javascript standard when dealing with arrays (returns indicies).
+		//  Note, follows javascript standard when dealing with arrays (
+		//  returns indicies).
 		Json::Value sub(const std::string& path) const;
 
 		//Pushes a val into an array located at path and resizes it to len.
 		//  Arrays here are treated as queues.
-		//  Similar functionality of set when dealing with existing key names in the given path.
+		//  Similar functionality of set when dealing with existing key names in
+		//  the given path.
 		void push(const std::string& path,const Json::Value& val,const Json::Value& len);
 
-		//Authenticates path with opts with the passed auth object (expects a string or null).
-		//  FIXME:  Authentication is hardcoded to 123...
-		bool auth_check(const std::string& path,const std::string& opts,const Json::Value& auth);
+		//Authenticates path with opts with the passed auth object
+		//  Note, expects a string or null in the auth object.
+		//  Note, recursive function, passing the original path around in
+		//        recursive_path...
+		//  Note, PATH IS STRIPPED OF ALL PRECEEDING /'s and ENDING /'s,
+		//        AND MULTIPLE /'s ARE CHANGED TO A SINGLE /.
+		bool auth_check(std::string path,const std::string& opts,
+			const Json::Value& auth,std::string recursive_path="");
 
 	private:
 		std::string auth_file_m;
