47dba950d55226bcc9d548ab3f2d3e4ab4f53d3f
Chris Bailey <chris@chrisbailey.io>
2016-07-19 00:00:15 -0800

    Was going to add tests, ended up refactoring.

--- utils/testing/superstar/main.py
+++ utils/testing/superstar/main.py
@@ -1,51 +1,81 @@
-import requests
+#!/usr/bin/env python3
 from sys import argv
+import requests
 from termcolor import colored
 import hmac
 from hashlib import sha256
 import json
 
-# This script reimplements JSONRPC/Superstar communication spec rather than 
-# using the existing Python library, to prevent changes to that library
-# from becoming de facto spec changes.
 
-##
-# Helper methods
-##
+# These classes should probably be in separate files eventually.
+class Test:
+    def __init__(self, url):
+        self.url = url
+        self.error = 'No error message provided.'
 
-def create_method(method_name, path, auth_key, **opts):
-    json_opts = json.dumps(opts)
-    auth = hmac.new(auth_key.encode(), json_opts.encode(), sha256).hexdigest()
-    params = {'path': path, 'opts': json_opts, 'auth': auth}
-    return {'jsonrpc': '2.0', 'method': method_name, 'params': params, 'id': None}
+    def create_method(self, method_name, path, auth_key, **opts):
+        json_opts = json.dumps(opts)
+        auth = hmac.new(
+            auth_key.encode(),
+            json_opts.encode(),
+            sha256
+        ).hexdigest()
+        params = {'path': path, 'opts': json_opts, 'auth': auth}
+        return {
+            'jsonrpc': '2.0',
+            'method': method_name,
+            'params': params,
+            'id': None
+        }
 
-def post_single_request(method):
-    return requests.post(argv[1], json.dumps(method)).json()
+    def post_single_request(self, method):
+        return requests.post(self.url, json.dumps(method)).json()
 
-def post_batch_request(*methods):
-    return requests.post(argv[1], methods).json()
+    def post_batch_request(self, *methods):
+        return requests.post(self.url, methods).json()
 
-def pass_test(test):
-    print(colored('Passed: ', 'green') + test)
+    def pass_test(self):
+        print(colored('Passed: ', 'green') + self.description)
+        print(self.error)
 
-def fail_test(test):
-    print(colored('Failed: ', 'red') + test)
+    def fail_test(self):
+        print(colored('Failed: ', 'red') + self.description)
+        print(self.error)
 
-##
-# Tests - each is a self-contained function, so that all main has to do is call them.
-##
+    # Test subclasses override this method with their test code.
+    # Passes if it returns true.
+    # Fails and prints self.error if it returns false
+    # Fails if an exception is raised, then catches and prints it.
+    def task(self):
+        pass
 
-def test_set_singular():
-    name = "Set value, nonbatched"
-    method = create_method('set', '/test/set/singular', '', value='testvalue')
-    print(post_single_request(method))
-    if post_single_request(method)['result']:
-        pass_test(name)
-    else:
-        fail_test(test)
+    # Runs task() and reports result
+    def run(self):
+        try:
+            if self.task():
+                self.pass_test()
+            else:
+                self.fail_test()
+        except Exception as exception:
+            self.error = exception
+            self.fail_test()
 
-    
+
+class SingleSet(Test):
+    def task(self):
+        self.description = "Set value, nonbatched"
+        method = self.create_method(
+            'set',
+            '/test/set/singular',
+            '',
+            value='testvalue'
+        )
+        return self.post_single_request(method)['result']
 
 
 if __name__ == '__main__':
-    test_set_singular()
+    if len(argv) > 1:
+        url = argv[1]
+        SingleSet(url).run()
+    else:
+        print('Missing URL argument.')
