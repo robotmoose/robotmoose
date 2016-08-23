05fcd127e0d129cef2f4f9ef0621062010fb2d1f
Mike Moss <mrmoss@alaska.edu>
2016-08-23 10:55:50 -0800

    Updated dependency script to install udev as well...

--- subsystems/install_deps_ubuntu.sh
+++ subsystems/install_deps_ubuntu.sh
@@ -9,3 +9,15 @@ cd build
 sudo cmake ..
 sudo make install
 sudo ldconfig
+sudo rm -f /etc/udev/rules.d/51-kinect.rules
+sudo echo '# ATTR{product}=="Xbox NUI Motor"'                                              |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02b0", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo '# ATTR{product}=="Xbox NUI Audio"'                                              |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ad", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo '# ATTR{product}=="Xbox NUI Camera"'                                             |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ae", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo ''                                                                               |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo '# Kinect for Windows'                                                           |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02c2", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02be", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02bf", MODE="0666"' |tee -a /etc/udev/rules.d/51-kinect.rules > /dev/null
