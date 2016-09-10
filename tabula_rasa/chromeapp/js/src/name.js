3b4b8de44f04e714dca99e6f9c9e7f3a3fed5ad6
Mike Moss <mrmoss@alaska.edu>
2016-09-10 15:45:07 -0800

    Removed old backup superstars...

--- tabula_rasa/chromeapp/js/src/name.js
+++ tabula_rasa/chromeapp/js/src/name.js
@@ -100,8 +100,6 @@ name_t.prototype.update_superstars_m=function()
 	[
 		"robotmoose.com",
 		"test.robotmoose.com",
-		"androidantelope.com",
-		"cyborgcaribou.com",
 		"127.0.0.1:8081",
 		"10.8.110.36:8081"
 	];
