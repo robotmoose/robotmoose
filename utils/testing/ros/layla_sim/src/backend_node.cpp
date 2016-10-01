0b5cd97c0c41b216cc22f75754da47689552f153
Ryker Dial <ryker.dial@gmail.com>
2016-09-30 20:06:38 -0800

    Adapted ros tests for new latency and bandwidth tests. Defaults to 1 KiB of data and 1 MiB of data.

--- utils/testing/ros/layla_sim/src/backend_node.cpp
+++ utils/testing/ros/layla_sim/src/backend_node.cpp
@@ -1,34 +1,51 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "layla_sim/duplex.h"
+#include "layla_sim/test_config.h"
 
-#include <sstream>
+#include <string>
 
-void frontendCallback(const std_msgs::String::ConstPtr & msg) {
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
+std::string latency_test_data;
+std::string bandwidth_test_data;
+size_t BYTES_LATENCY_TEST = 1024; // 1 KiB
+size_t BYTES_BANDWIDTH_TEST = 1024*1024; // 1 MiB
+
+bool test_config(layla_sim::test_config::Request &req, layla_sim::test_config::Response &res) {
+	res.bytes_latency_test = BYTES_LATENCY_TEST;
+	res.bytes_bandwidth_test = BYTES_BANDWIDTH_TEST;
+	return true;
+}
+
+bool test_latency(layla_sim::duplex::Request &req, layla_sim::duplex::Response &res) {
+	res.response = latency_test_data;
+	return true;
+}
+
+bool test_bandwidth(layla_sim::duplex::Request &req, layla_sim::duplex::Response &res) {
+	res.response = bandwidth_test_data;
+	return true;
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "backend_node");
+
+	if(argc > 1)
+		BYTES_LATENCY_TEST=std::stoi(argv[1]);
+	if(argc > 2)
+		BYTES_BANDWIDTH_TEST=std::stoi(argv[1]);
+
+	ROS_INFO("Bytes to send for latency tests: %lu", BYTES_LATENCY_TEST);
+	ROS_INFO("Bytes to send for bandwidth tests: %lu", BYTES_BANDWIDTH_TEST);
+
+	latency_test_data = std::string(BYTES_LATENCY_TEST, 'B');
+	bandwidth_test_data = std::string(BYTES_BANDWIDTH_TEST, 'B');
+
 	ros::NodeHandle n;
-	ros::Publisher backend_pub = n.advertise<std_msgs::String>("layla_backend", 1000);
-	ros::Subscriber frontend_sub = n.subscribe("layla_frontend",  1000, frontendCallback);
-	ros::Rate loop_rate(10);
 
-	int count = 0;
-	while(ros::ok()) {
-		std_msgs::String msg;
-		std::stringstream ss;
-		ss << "Hello world from the backend! " << count;
+	ros::ServiceServer test_config_service = n.advertiseService("test_config", test_config);
+	ros::ServiceServer test_latency_service = n.advertiseService("test_latency", test_latency);
+	ros::ServiceServer test_bandwidth_service = n.advertiseService("test_bandwidth", test_bandwidth);
+	ROS_INFO("Ready to test latency and bandwidth.");
+	ros::spin();
 
-		msg.data = ss.str();
-		ROS_INFO("%s", msg.data.c_str());
-
-		backend_pub.publish(msg);
-
-		ros::spinOnce();
-
-		loop_rate.sleep();
-		++count;
-	}
 	return 0;
 }
--- utils/testing/ros/layla_sim/src/frontend_node.cpp
+++ utils/testing/ros/layla_sim/src/frontend_node.cpp
@@ -1,34 +1,71 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "layla_sim/duplex.h"
+#include "layla_sim/test_config.h"
 
-#include <sstream>
+#include <chrono>
+#include <string>
+#include <thread>
 
-void backendCallback(const std_msgs::String::ConstPtr & msg) {
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
+std::string latency_test_data;
+std::string bandwidth_test_data;
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "frontend_node");
+
+	size_t ITERS=1000;
+
+	if(argc>1)
+		ITERS=std::stoi(argv[1]);
+
+	ROS_INFO("Iterations: %lu", ITERS);
+
 	ros::NodeHandle n;
-	ros::Publisher frontend_pub = n.advertise<std_msgs::String>("layla_frontend", 1000);
-	ros::Subscriber backend_sub = n.subscribe("layla_backend", 1000, backendCallback);
-	ros::Rate loop_rate(10);
 
-	int count = 0;
-	while(ros::ok()) {
-		std_msgs::String msg;
-		std::stringstream ss;
-		ss << "Hello world from the frontend! " << count;
+	// Get the amount of data to send for each test from the backend node
+	ros::ServiceClient test_config_client = n.serviceClient<layla_sim::test_config>("test_config");
+	layla_sim::test_config test_config_srv;
+	test_config_client.call(test_config_srv);
 
-		msg.data = ss.str();
-		ROS_INFO("%s", msg.data.c_str());
+	latency_test_data  = std::string(test_config_srv.response.bytes_latency_test, 'A');
+	bandwidth_test_data  = std::string(test_config_srv.response.bytes_bandwidth_test, 'A');
 
-		frontend_pub.publish(msg);
+	ROS_INFO("Bytes to send for latency tests: %lu", test_config_srv.response.bytes_latency_test);
+	ROS_INFO("Bytes to send for bandwidth tests: %lu\n", test_config_srv.response.bytes_bandwidth_test);
 
-		ros::spinOnce();
+	//
 
-		loop_rate.sleep();
-		++count;
+	ros::ServiceClient test_latency_client = n.serviceClient<layla_sim::duplex>("test_latency");
+	ros::ServiceClient test_bandwidth_client = n.serviceClient<layla_sim::duplex>("test_bandwidth");
+	layla_sim::duplex test_srv;
+
+	// Perform the actual tests
+	ROS_INFO("Testing Network Latency.");
+	test_srv.request.data = latency_test_data;
+	std::chrono::duration<double> total_test_latency=std::chrono::duration<double>::zero();
+	for(size_t i=0; i<ITERS; ++i) {
+		auto t0 = std::chrono::high_resolution_clock::now();
+		test_latency_client.call(test_srv);
+		auto t1 = std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		total_test_latency += t1-t0;
 	}
+	ROS_INFO("Total Time (s): %lf", total_test_latency.count());
+	ROS_INFO("Average Time Per Call (ms): %lf\n", total_test_latency.count()/(double)ITERS*1000);
+
+
+	ROS_INFO("Testing Network Bandwidth.");
+	test_srv.request.data = bandwidth_test_data;
+	std::chrono::duration<double> total_test_bandwidth=std::chrono::duration<double>::zero();
+	for(size_t i=0; i<ITERS; ++i) {
+		auto t0 = std::chrono::high_resolution_clock::now();
+		test_bandwidth_client.call(test_srv);
+		auto t1 = std::chrono::high_resolution_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		total_test_bandwidth += t1-t0;
+	}
+	ROS_INFO("Total Time (s): %lf", total_test_bandwidth.count());
+	ROS_INFO("Total Time Per Call (ms): %lf\n", total_test_bandwidth.count()/(double)ITERS*1000);
+
 	return 0;
 }
