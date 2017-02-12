ebbe068f25ae77c2fe7368c04c8b117e160ab5cd
Chris Bailey <chris@chrisbailey.io>
2017-02-11 15:25:14 -0900

    Show icons for real robots

--- www/js/robot_ui.js
+++ www/js/robot_ui.js
@@ -212,17 +212,17 @@ robot_ui_t.prototype.download_gui=function()
 		{
 			_this.doorways=
 			{
-				config:_this.create_doorway("Configure","Set up robot hardware",help_text_config),
-				pilot:_this.create_doorway("Drive","Manually drive the robot",help_text_pilot),
-				sensors:_this.create_doorway("Sensors","Examine sensor data from robot",help_text_sensors),
-				charts:_this.create_doorway("Charts", "Chart sensor data received from robot",null),
-				states:_this.create_doorway("Code","Automatically drive the robot",help_text_states),
-				map:_this.create_doorway("Map","See where the robot thinks it is",help_text_map),
-				navigation: _this.create_doorway("Navigation", "Plan a path for the robot", null),
-				video:_this.create_doorway("Video","Show the robot's video camera",null),
-				UI:_this.create_doorway("UI","Customized robot user interface",help_text_ui),
-				sound:_this.create_doorway("Sound","Play sounds on the backend to get attention",null),
-				chat:_this.create_doorway("Chat","Chat with the caretaker of the robot.",null)
+				config:_this.create_doorway("Configure","Set up robot hardware",help_text_config, 'wrench'),
+				pilot:_this.create_doorway("Drive","Manually drive the robot",help_text_pilot, 'dashboard'),
+				sensors:_this.create_doorway("Sensors","Examine sensor data from robot",help_text_sensors, 'scale'),
+				charts:_this.create_doorway("Charts", "Chart sensor data received from robot",null, 'stats'),
+				states:_this.create_doorway("Code","Automatically drive the robot",help_text_states, 'list-alt'),
+				map:_this.create_doorway("Map","See where the robot thinks it is",help_text_map, 'globe'),
+				navigation: _this.create_doorway("Navigation", "Plan a path for the robot", null, 'road'),
+				video:_this.create_doorway("Video","Show the robot's video camera",null, 'facetime-video),
+				UI:_this.create_doorway("UI","Customized robot user interface",help_text_ui, 'object-align-top'),
+				sound:_this.create_doorway("Sound","Play sounds on the backend to get attention",null, 'volume-up'),
+				chat:_this.create_doorway("Chat","Chat with the caretaker of the robot.",null, 'comment')
 			};
 
 			clear_out(_this.doorways.config.content);
