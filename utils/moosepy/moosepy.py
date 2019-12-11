f6b63750cc913a4b2bac7a60d50db5d47780cedd
RyanStonebraker <ryanastonebraker@gmail.com>
2019-12-10 21:11:24 -0900

    Cleans up Robot class and takes out hardcoded parameters

--- utils/moosepy/moosepy.py
+++ utils/moosepy/moosepy.py
@@ -1,8 +1,17 @@
-# robot.py
-# Robot class controlling drive communication
-# 10/15/2019
-# Tristan Van Cise
-# Modified Dain Harmon 12/10/2019
+### Copyright 2019 University of Alaska Fairbanks
+##
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+##
+###     http://www.apache.org/licenses/LICENSE-2.0
+##
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
 
 import hashlib
 import hmac
@@ -10,11 +19,11 @@ import json
 import requests
 
 class Robot:
-
-    def __init__(self, password, verbose=False):
+    def __init__(self, superstar_url, superstar_path, password, verbose=False):
         self.password = password
         self.verbose = verbose
-        self.path = "robots/2019-09/uaf/coffeebot/pilot"
+        self.path = superstar_path
+        self.superstar_url = superstar_url
         self.opts = {
             "value": {
                 "power": {
@@ -33,24 +42,29 @@ class Robot:
             "jsonrpc": "2.0",
             "method": "set",
             "params": {
-                "path":"robots/2019-09/uaf/coffeebot/pilot",
-                "opts":"{\"value\":{\"power\":{\"L\":10,\"R\":10},\"trim\":0,\"time\":0,\"cmd\":{\"run\":\"\",\"arg\":\"\"}}}",
-                "auth":"46b641297dcf6facbcc8d976825ba794ce2b277a2d6b31294f2bd72c0d329faf"
+                "path": self.superstar_path,
+                "opts": "",
+                "auth": ""
             },
             "id":4
         }
 
+
     def setPassword(self, newPass):
         self.password = newPass
+
 
     def setPath(self, newPath):
         self.path = newPath
 
+
     def setOpts(self, newOpts):
         self.opts = newOpts
 
+
     def setVerbosity(self, value):
         self.verbose = value
+
 
     def getAuth(self):
         formatedPass = hashlib.sha256()
@@ -65,31 +79,37 @@ class Robot:
         
         return auth.hexdigest()
 
+
     def setMotorSpeed(self, leftMotorPower, rightMotorPower):
         self.opts["value"]["power"]["L"] = leftMotorPower
         self.opts["value"]["power"]["R"] = rightMotorPower
+
 
     def setRequestParams(self):
         self.request["params"]["path"] = str(self.path)
         self.request["params"]["opts"] = json.dumps(self.opts, separators=(',', ':'))
         self.request["params"]["auth"] = str(self.getAuth())
 
+
     def sendRequest(self):
         if self.verbose:
-            print("\nRequest Data:")
-            print(json.dumps(self.request, indent=4, sort_keys=True))
-            print("\n")
+            print("\nRequest Data:\n", json.dumps(self.request, indent=4, sort_keys=True), "\n")
             
         data = [self.request]
-        requests.post("https://robotmoose.com/superstar", json=data)
+        requests.post(self.superstar_url, json=data)
+
 
     def drive(self, leftMotorPower, rightMotorPower):
         self.setMotorSpeed(leftMotorPower, rightMotorPower)
         self.setRequestParams()
 
         if self.verbose:
-            print("Motor power set to:")
-            print("Left: {}% \nRight: {}%".format(leftMotorPower,rightMotorPower))
-            print("\n")
+            print("Motor power set to:", "Left: {}% \nRight: {}%".format(leftMotorPower,rightMotorPower), "\n")
 
         self.sendRequest()
+
+
+if __name__ == "__main__":
+    superstar_path = "robots/2019-09/uaf/coffeebot/pilot"
+    password = "example_password"
+    robot = Robot(superstar_path, password)
